554e059f87d90288dc7bef829f590c1418e1e6ed
Vitezslav Homolka <homolka@1024.cz>
2022-04-06 11:19:06 +0200

    nvim: add lua colorscheme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,6 +4,12 @@
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  -- [[ UI ]]
+
+  use {
+    'ishan9299/nvim-solarized-lua'
+  }
+
   use {                                              
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons'
--- .config/nvim/newinit.lua
+++ .config/nvim/newinit.lua
@@ -4,3 +4,7 @@ require('plugins')
 
 require'nvim-tree'.setup {}
 
+vim.o.termguicolors = true
+vim.o.background = 'light'
+
+vim.cmd('colorscheme solarized')
