4115aded1dcff53cdf021010f6d8900fdc9670a2
Vitezslav Homolka <homolka@1024.cz>
2022-05-23 20:18:27 +0200

    nvim: add basic telescope config

--- .config/nvim/lua/setups/telescope.lua
+++ .config/nvim/lua/setups/telescope.lua
@@ -1,10 +1,15 @@
-
 require('telescope').setup({
   defaults = {
+    layout_strategy = 'vertical',
     layout_config = {
-      vertical = { width = 0.5 }
+      vertical = {
+        mirror = true,
+        width = 0.95,
+        height = 0.95
+     }
     }
   }
 })
 
-
+vim.api.nvim_set_keymap('', '<C-p>', ':Telescope find_files<cr>', {})
+vim.api.nvim_set_keymap('', '<C-s>', ':Telescope live_grep<cr>', {})
