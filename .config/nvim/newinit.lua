8e28f2f2bbe89f649f8704e1adbf3656d6c6ce02
Vitezslav Homolka <homolka@1024.cz>
2022-06-04 22:28:19 +0200

    nvim: add basic mappings

--- .config/nvim/newinit.lua
+++ .config/nvim/newinit.lua
@@ -21,3 +21,19 @@ vim.wo.signcolumn = 'yes'
 
 vim.cmd [[colorscheme solarized]]
 
+vim.keymap.set("n", "<leader>s", ":wall<cr>")
+vim.keymap.set("n", "<leader>q", ":bdelete<cr>")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>d", "\"+d")
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("n", "<leader>P", "\"+P")
+
+vim.keymap.set("n", "<c-h>", "<c-w><c-h>")
+vim.keymap.set("n", "<c-j>", "<c-w><c-j>")
+vim.keymap.set("n", "<c-k>", "<c-w><c-k>")
+vim.keymap.set("n", "<c-l>", "<c-w><c-l>")
+vim.keymap.set("n", "<tab>", "<c-w>w")
+vim.keymap.set("n", "<s-tab>", "<c-w>p")
