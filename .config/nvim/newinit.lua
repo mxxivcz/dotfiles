91d2c2ad849bdfe32528bcce46984ffdc263118b
Vitezslav Homolka <homolka@1024.cz>
2022-06-04 22:27:49 +0200

    nvim: add basic options

--- .config/nvim/newinit.lua
+++ .config/nvim/newinit.lua
@@ -3,10 +3,19 @@ require('plugins')
 require('setups')
 
 vim.g.mapleader = ' '
-vim.g.maplocalleader = '\t'
 
 vim.opt.termguicolors = true
 vim.opt.background = 'light'
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.suffixesadd= ".js,.jsx,.ts,.tsx,.lua"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.undodir = vim.fn.getenv('HOME') .. '.local/share/nvim/undodir'
 
 vim.wo.signcolumn = 'yes'
 
