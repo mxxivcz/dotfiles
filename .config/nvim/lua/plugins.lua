4f9c2fab2c2dfe524e982579fd244abfa3198f9d
Vitezslav Homolka <homolka@1024.cz>
2022-08-02 17:33:00 +0200

    nvim: add markdown preview plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -53,4 +53,9 @@ return require('packer').startup(function(use)
 
   use 'phaazon/hop.nvim'
 
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+})
+
 end)
