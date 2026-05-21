52ac569971521e3ee39d6bde7a87450792debbda
Vítězslav Homolka <homolka@1024.cz>
2026-05-21 12:19:06 +0200

    nvim: keep only kulala for http requests

--- .config/nvim/lua/plugins/http.lua
+++ .config/nvim/lua/plugins/http.lua
@@ -1,21 +1,10 @@
 return {
 	{
 		"mistweaverco/kulala.nvim",
-		opts = {},
-	},
-	{
-		"diepm/vim-rest-console",
-		config = function()
-			vim.g.vrc_trigger = "<leader><enter>"
-			vim.g.vrc_response_default_content_type = "application/json"
-			vim.g.vrc_horizontal_split = vim.api.nvim_win_get_height(0) > vim.api.nvim_win_get_width(0)
-			vim.g.vrc_curl_opts = {
-				["-sS"] = "",
-				["--connect-timeout"] = 10,
-				-- ["-i"] = "",
-				["--max-time"] = 60,
-				["-k"] = "",
-			}
-		end,
+		ft = { "http", "rest" },
+		opts = {
+			global_keymaps = true,
+			winbar = false,
+		},
 	},
 }
